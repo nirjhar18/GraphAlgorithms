b5ebd9439c130b2ac3246db36ead5add3fd601d3
Nirjhar Vermani <nirjhar18@gmail.com>
2018-10-22 16:41:11 -0500

    Add files via upload

---
+++ GraphsWithAdjacentList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithms
+{
+    public class GraphsWithAdjacentList
+    {
+
+        public int V; // Number of vertices
+        public List<int>[] adj; //Array of Lists
+
+        public GraphsWithAdjacentList() { }
+
+        //Constructor for new adjacent list
+        public GraphsWithAdjacentList(int vertices)
+        {
+            V = vertices;
+            adj = new List<int>[V];
+            for (int i = 0; i < V; i++)
+            {
+                adj[i] = new List<int>();
+
+            }
+        }
+
+        //Use an array of Lists
+        // 0 - > List ()
+        // 0 -> 1 - > 2
+        // 1 - > 2
+        public void AddEdge(int a, int b)
+        {
+            adj[a].Add(b);
+        }
+
+        //Print Edges 
+        public void PrintEdge()
+        {
+            for (int i = 0; i < adj.Length; i++)
+
+            {
+                foreach (var j in adj[i])
+                {
+                    Console.WriteLine(i + "->" + j);
+                }
+            }
+
+        }
+
+        //Find the index of most adjacent neighbors
+        public int MostNeighbors()
+        {
+
+            int[] finalCount = new int[adj.Length];
+            for (int i = 0; i < adj.Length; i++)
+
+            {
+                finalCount[i] = adj[i].Count;
+
+            }
+
+            return Array.IndexOf(finalCount, finalCount.Max());
+
+        }
+
+        //Breadth First Search of a vertice
+        // We have an array to check if the vertice/node has already been visisted. I am using a queue to keep
+        // vertices. We start with input vertice and add it to queue and change the flag to visited.
+        // Then we dequeue and print that vertice and loop through the list of that vertice to find its adjacent
+        // nodes/neighbors. We add it to queue and then dequeue the first element and print it 
+        // and then we loop through to find the adjacent members of the de-queued vertice.
+        //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+
+        public virtual void BFS(int vertice) { }
+
+    }
+}
---
+++ Traversals.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithms
+{
+    class Traversals : GraphsWithAdjacentList
+    {
+
+        //Breadth First Search of a vertice
+        // We have an array to check if the vertice/node has already been visisted. I am using a queue to keep
+        // vertices. We start with input vertice and add it to queue and change the flag to visited.
+        // Then we dequeue and print that vertice and loop through the list of that vertice to find its adjacent
+        // nodes/neighbors. We add it to queue and then dequeue the first element and print it 
+        // and then we loop through to find the adjacent members of the de-queued vertice.
+        //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+
+        public override void BFS(int vertice)
+        {
+            bool[] visited = new bool[5];
+            Queue<int> vertices = new Queue<int>();
+            vertices.Enqueue(vertice);
+            visited[vertice] = true;
+
+
+            while (vertices.Count != 0)
+            {
+
+                vertice = vertices.Dequeue();
+                Console.Write(vertice + " ");
+                foreach (var i in adj[vertice])
+                {
+                    if (visited[i] == false)
+                    {
+                        visited[i] = true;
+                        vertices.Enqueue(i);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
